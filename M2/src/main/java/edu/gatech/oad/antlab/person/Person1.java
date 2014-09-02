515277cea5875517ca9dd1d7ddecf412ebd782c2
baodvu <baodvu@gmail.com>
2014-09-01 22:30:16 -0400

    Implement Person 1 method

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -31,6 +31,10 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
+	  if (input.length() >= 3) {
+		  String result = input.substring(2) + input.substring(0,2);
+		  return result;
+	  }
 	  return null;
 	}
 	
