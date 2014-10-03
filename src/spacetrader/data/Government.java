13172f9a4aac64644fb7ac1b5b5c419edcb176d9
MichaelALong <michael.long.atlanta@gmail.com>
2014-10-02 21:56:48 -0400

    includes traveling methods and new Government enum

---
+++ src/spacetrader/data/Government.java
@@ -0,0 +1,48 @@
+package spacetrader.data;
+
+public enum Government {
+	
+	ANARCHY(0, "Anarchy"),
+	CAPITALIST(1, "Capitalist State"),
+	COMMUNIST(2, "Communist State"),
+	CONFEDERACY(3, "Confederacy State"),
+	CORPORATE(4, "Corporate State"),
+	CYBERNETIC(5, "Cybernetic State"),
+	DEMOCRACY(6, "Democracy State"),
+	DICTATORSHIP(7, "Dictatorship"),
+	FASCIST(8, "Fascist State"),
+	FEUDAL(9, "Feudal State"),
+	MILITARY(10, "Military State"),
+	MONARCHY(11, "Monarchy"),
+	PACIFIST(12, "Pacifist State"),
+	SOCIALIST(13, "Socialist State"),
+	SATORI(14, "State of Satori"),
+	TECHNOCRACY(15, "Technocracy"),
+	THEOCRACY(16, "Theocracy");
+	
+	private int value;
+    private String text;
+
+    private Government(int _value, String _text) {
+        this.value = _value;
+        this.text = _text;
+    }
+    
+    /**
+     * Returns the text representation of this Government
+     */
+    @Override
+    public String toString() {
+        return this.text;
+    }
+
+    /**
+     * Gets the value of the Government
+     *
+     * @return numerical value for this type of Government
+     */
+    public int getValue() {
+        return this.value;
+    }
+
+}
