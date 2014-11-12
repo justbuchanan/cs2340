3dc928ee0c3cf5170a03a44cf7de45ddb8bb773f
baodvu <baodvu@gmail.com>
2014-11-12 12:16:22 -0500

    Add more tests

--- test/spacetrader/controllers/market/CommandProcessorTest.java
+++ test/spacetrader/controllers/market/CommandProcessorTest.java
@@ -12,32 +12,62 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import spacetrader.data.Item;
+import spacetrader.data.Resource;
+import spacetrader.data.TechLevel;
 import spacetrader.models.Marketplace;
 import spacetrader.models.Player;
+import spacetrader.models.SolarSystem;
 
 /**
- * Tests for Command Processor
+ * This JUnit tests the command processor to ensure the buy item command
+ * works properly
  * @author Bao
  */
 public class CommandProcessorTest {
     private Player myPlayer;
     private Marketplace myMarket;
+    private SolarSystem mySS;
     
+    /**
+     * Creates player and marketplace objects needed for the tests
+     */
     @Before
     public void setUp() {
         myPlayer = new Player("Tester", new int[]{0, 0, 0, 0, 0});
-        myPlayer.setBalance(1000);
-        myMarket = null;
+        myPlayer.setBalance(10000);
+        mySS = new SolarSystem();
+        mySS.setResource(Resource.DESERT);
+        mySS.setTechLevel(TechLevel.MEDIEVAL);
+        myMarket = mySS.setMP();
     }
+    
+    /**
+     * This test checks the following:
+     * Market price is correct
+     * Total = price * balance
+     * Player's balance is deducted
+     * Item is added to the ship
+     * 
+     * It also test undo and redo commands
+     */
     @Test
     public void testAddCommand() {
         CommandProcessor cp = AbstractCommand.INSTANCE;
-        Item item = Item.FIREARMS;
+        Item item = Item.WATER;
+        //Resets the cargo bays to zero
+        myPlayer.getShip().getCargo().set(Item.WATER.getValue(), 0);
         int quantity = 3;
         BuyItemCommand cmd = new BuyItemCommand(myPlayer, myMarket, item, quantity);
+        int initialBalance = myPlayer.getBalance();
         cp.doCommand(cmd);
+        assertEquals("Balance was not deducted correctly", initialBalance - myMarket.getBuyPrice(item)*quantity, myPlayer.getBalance());
+        assertEquals("Item was not added", new Integer(3), myPlayer.getShip().getCargo().get(item.getValue()));
         cp.undoCommand();
+        assertEquals("Undo command did not restore player's balance", initialBalance, myPlayer.getBalance());
+        assertEquals("Item was not removed", new Integer(0), myPlayer.getShip().getCargo().get(item.getValue()));
         cp.redoCommand();
+        assertEquals("Redo command did not execute correctly", initialBalance - myMarket.getBuyPrice(item)*quantity, myPlayer.getBalance());
+        assertEquals("Item was not added", new Integer(3), myPlayer.getShip().getCargo().get(item.getValue()));
     }
     
 }
